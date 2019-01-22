a361b7ba1e3da8a1e53b6186353a294dac2c1924
xuanmiao-zero <1297146548@qq.com>
2019-01-22 14:05:24 +0800

    添加isIphoneXR判断

--- device/index.js
+++ device/index.js
@@ -2,28 +2,32 @@ import { Dimensions, Platform, PixelRatio } from 'react-native';
 
 const dimen = Dimensions.get('window');
 
-// iPhoneX
+// iPhoneX, XS
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
-const XR_MAX_WIDTH = 414;
-const XR_MAX_HEIGHT = 896;
+
+// iPhoneXR, XSMax
+const XR_WIDTH = 414;
+const XR_HEIGHT = 896;
 
 const DEVICE_WIDTH = dimen.width;
 const DEVICE_HEIGHT = dimen.height;
 const isIOS = Platform.OS === 'ios';
 const fixSize = size => PixelRatio.roundToNearestPixel((size / 375) * DEVICE_WIDTH);
 const onePixel = 1 / PixelRatio.get();
-const isIphoneX = (isIOS &&
+const isIphoneX = isIOS &&
   ((DEVICE_HEIGHT === X_HEIGHT && DEVICE_WIDTH === X_WIDTH) ||
-    (DEVICE_HEIGHT === X_WIDTH && DEVICE_WIDTH === X_HEIGHT))) || (isIOS &&
-  ((DEVICE_HEIGHT === XR_MAX_HEIGHT && DEVICE_WIDTH === XR_MAX_WIDTH) ||
-    (DEVICE_HEIGHT === XR_MAX_WIDTH && DEVICE_WIDTH === XR_MAX_HEIGHT)));
+  (DEVICE_HEIGHT === X_WIDTH && DEVICE_WIDTH === X_HEIGHT))
+const isIphoneXR = isIOS &&
+  ((DEVICE_HEIGHT === XR_HEIGHT && DEVICE_WIDTH === XR_WIDTH) ||
+  (DEVICE_HEIGHT === XR_WIDTH && DEVICE_WIDTH === XR_HEIGHT))
 
 export {
   DEVICE_WIDTH,
   DEVICE_HEIGHT,
   onePixel,
   isIphoneX,
+  isIphoneXR,
   fixSize,
   isIOS,
 };
